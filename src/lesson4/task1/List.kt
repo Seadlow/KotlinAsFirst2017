45e7999a05ff681088f54170abacaa4a7db67292
Vasiliy_Macsyom <seadlow147@gmail.com>
2017-11-02 00:43:27 +0300

    Troubleshooting and second part of lesson 4 Мне пришлось делать еще один коммит из-за бесконечного цикла в одной из недоделанных задач в уроке 4, в результате чего ни одна из задач не проверена. Простите

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -220,14 +220,14 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> {
-    val list = mutableListOf<Int>()
-    while (n > base) {
-        list.add(n % base)
-    }
-    list.add(1)
-    return list
-}
+fun convert(n: Int, base: Int): List<Int> = TODO()
+//    val list = mutableListOf<Int>()
+//    while (n > base) {
+//        list.add(n % base)
+//    }
+//    list.add(1)
+//    return list
+//}
 
 /**
  * Сложная
